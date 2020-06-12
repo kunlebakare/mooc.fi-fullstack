930f3c49664345e3104ac6dded85f20afbec56d2
@kunle_codes <49395667+kunlebakare@users.noreply.github.com>
2020-06-12 15:43:29 +0100

    Update index.js
    refactored component: Content
    added component: Part

--- part 1/myapp/src/index.js
+++ part 1/myapp/src/index.js
@@ -1,6 +1,46 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = (props) => {
+  return (
+    <div>
+      <h1>
+        {props.name}
+      </h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.courses} {props.exercises}
+      </p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part courses={props.courses1} exercises={props.ex1} />
+      <Part courses={props.courses2} exercises={props.ex2} />
+      <Part courses={props.courses3} exercises={props.ex3} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+        {props.title} {props.ex1 + props.ex2 + props.ex3}
+      </p>
+    </div>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -12,17 +52,11 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header name={course} />
+      <Content  courses1={part1} ex1={exercises1}
+                courses2={part2} ex2={exercises2}
+                courses3={part3} ex3={exercises3} />
+      <Total title="Number of exercises" ex1={exercises1} ex2={exercises2} ex3={exercises3} />
     </div>
   )
 }
